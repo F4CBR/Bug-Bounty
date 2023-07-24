bf3fa3f66c94f5ec9fb9071bbab634a5dfc408ef
F4CBR <49267296+F4CBR@users.noreply.github.com>
2023-07-24 23:08:43 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,70 +1,54 @@
 #!/bin/bash
 
-clear
-echo "Penginstallan tools Bug Bounty"
-echo "=============================="
-sleep 2
+# Function to print green text
+function print_green() {
+  echo -e "\033[1;32m$1\033[0m"
+}
+
+clear; print_green "Penginstallan tools Bug Bounty\n=============================="; sleep 2
+
+# Function to check if a tool is installed
+function is_tool_installed() {
+  if which "$1" &>/dev/null; then
+    return 0  # Tool is installed
+  else
+    return 1  # Tool is not installed
+  fi
+}
 
 # Update and Upgrade the system
-clear
-echo "Memperbarui paket dan sistem..."
-sudo apt update -y
-sudo apt upgrade -y
+print_green "Memperbarui paket dan sistem..."; sudo apt update && sudo apt upgrade
 
 # install subfinder
-clear
-echo "Menginstall Subfinder"
-sleep 2
-git clone https://github.com/projectdiscovery/subfinder.git
-cd subfinder/v2/cmd/subfinder
-go build .
-mv subfinder /usr/local/bin/
+if ! is_tool_installed "subfinder"; then
+  clear; print_green "Menginstall Subfinder"; sleep 2; git clone https://github.com/projectdiscovery/subfinder.git && cd subfinder/v2/cmd/subfinder && go build . && sudo mv subfinder /usr/local/bin/
+fi
 
 # install httpx
-clear
-echo "Menginstall httpx"
-sleep 2
-git clone https://github.com/projectdiscovery/httpx.git; cd httpx/cmd/httpx; go build; mv httpx /usr/local/bin/
+if ! is_tool_installed "httpx"; then
+  clear; print_green "Menginstall httpx"; sleep 2; git clone https://github.com/projectdiscovery/httpx.git && cd httpx/cmd/httpx && go build && sudo mv httpx /usr/local/bin/
+fi
 
 # install nuclei
-clear
-echo "Menginstall Nuclei"
-sleep 2
-git clone https://github.com/projectdiscovery/nuclei.git
-cd nuclei/v2/cmd/nuclei/
-go build .
-mv nuclei /usr/local/bin/
-nuclei -update-templates
-nuclei -update
+if ! is_tool_installed "nuclei"; then
+  clear; print_green "Menginstall Nuclei"; sleep 2; git clone https://github.com/projectdiscovery/nuclei.git && cd nuclei/v2/cmd/nuclei/ && go build . && sudo mv nuclei /usr/local/bin/ && nuclei -update-templates && nuclei -update
+fi
 
 # Install Paramspider
-clear
-echo "Menginstall Paramspider"
-sleep 2
-git clone https://github.com/devanshbatham/ParamSpider.git
-cd ParamSpider
-pip3 install -r requirements.txt
+if ! is_tool_installed "paramspider"; then
+  clear; print_green "Menginstall Paramspider"; sleep 2; git clone https://github.com/devanshbatham/ParamSpider.git && cd ParamSpider && pip3 install -r requirements.txt
+  sudo ln -s "$(pwd)/paramspider.py" /usr/local/bin/paramspider
+fi
 
 # install dalfox
-clear
-echo "Menginstall Dalfox"
-sleep 2
-git clone https://github.com/hahwul/dalfox
-cd dalfox
-go install
-go build
-cp /home/parrot/go/bin/dalfox /usr/local/bin
+if ! is_tool_installed "dalfox"; then
+  clear; print_green "Menginstall Dalfox"; sleep 2; git clone https://github.com/hahwul/dalfox && cd dalfox && go install && go build && sudo cp /home/parrot/go/bin/dalfox /usr/local/bin/
+fi
 
 # install kxss
-clear
-echo "Menginstall kxss"
-sleep 2
-git clone https://github.com/Emoe/kxss.git
-cd kxss
-go mod init kxss
-go get github.com/Emoe/kxss
+if ! is_tool_installed "kxss"; then
+  clear; print_green "Menginstall kxss"; sleep 2; git clone https://github.com/Emoe/kxss.git && cd kxss && go mod init kxss &>/dev/null && go mod tidy &>/dev/null && go get github.com/Emoe/kxss && go install github.com/Emoe/kxss
+fi
 
 # Instalasi selesai
-clear
-sleep 2
-echo "Instalasi selesai"
+clear; sleep 2; print_green "Instalasi selesai"
