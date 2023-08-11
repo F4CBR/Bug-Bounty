f9b7f920c3811acd76bb8a51e08b98a25be2aca7
F4CBR <49267296+F4CBR@users.noreply.github.com>
2023-08-11 13:26:49 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -33,14 +33,45 @@ function is_tool_installed() {
   fi
 }
 
-# Change to the target directory
-cd /home/parrot/Tools
-
 # Update and Upgrade the system
 print_green "Memperbarui paket dan sistem..."; sudo apt update && sudo apt upgrade
 
-# ... rest of your installation code ...
+# install subfinder
+if ! is_tool_installed "subfinder"; then
+  clear; print_green "Menginstall Subfinder"; sleep 2; git clone https://github.com/projectdiscovery/subfinder.git && cd subfinder/v2/cmd/subfinder && go build . && sudo mv subfinder /usr/local/bin/
+  cd "$SCRIPT_DIR"  # Navigate back to the main installation directory
+fi
+
+# install httpx
+if ! is_tool_installed "httpx"; then
+  clear; print_green "Menginstall httpx"; sleep 2; git clone https://github.com/projectdiscovery/httpx.git && cd httpx/cmd/httpx && go build && sudo mv httpx /usr/local/bin/
+  cd "$SCRIPT_DIR"  # Navigate back to the main installation directory
+fi
+
+# install nuclei
+if ! is_tool_installed "nuclei"; then
+  clear; print_green "Menginstall Nuclei"; sleep 2; git clone https://github.com/projectdiscovery/nuclei.git && cd nuclei/v2/cmd/nuclei/ && go build . && sudo mv nuclei /usr/local/bin/ && nuclei -update-templates && nuclei -update
+  cd "$SCRIPT_DIR"  # Navigate back to the main installation directory
+fi
+
+# Install Paramspider
+if ! is_tool_installed "paramspider"; then
+  clear; print_green "Menginstall Paramspider"; sleep 2; git clone https://github.com/devanshbatham/ParamSpider.git && cd ParamSpider && pip3 install -r requirements.txt
+  sudo ln -s "$(pwd)/paramspider.py" /usr/local/bin/paramspider
+  cd "$SCRIPT_DIR"  # Navigate back to the main installation directory
+fi
+
+# install dalfox
+if ! is_tool_installed "dalfox"; then
+  clear; print_green "Menginstall Dalfox"; sleep 2; git clone https://github.com/hahwul/dalfox && cd dalfox && go install && go build && sudo cp /home/parrot/go/bin/dalfox /usr/local/bin/
+  cd "$SCRIPT_DIR"  # Navigate back to the main installation directory
+fi
+
+# install kxss
+if ! is_tool_installed "kxss"; then
+  clear; print_green "Menginstall kxss"; sleep 2; git clone https://github.com/Emoe/kxss.git && cd kxss && go mod init kxss &>/dev/null && go mod tidy &>/dev/null && go get github.com/Emoe/kxss && go install github.com/Emoe/kxss
+  cd "$SCRIPT_DIR"  # Navigate back to the main installation directory
+fi
 
 # Instalasi selesai
-cd "$SCRIPT_DIR"  # Navigate back to the main installation directory
 clear; sleep 2; print_green "Instalasi selesai"
