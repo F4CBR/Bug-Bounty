c269fbc6692f6599579ea971d356e93e3583fbcf
F4CBR <49267296+F4CBR@users.noreply.github.com>
2024-08-10 00:05:41 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -75,7 +75,7 @@ if ! is_tool_installed "kxss"; then
   clear; print_green "Menginstall kxss"; sleep 2; 
   git clone https://github.com/Emoe/kxss.git && 
   cd kxss && go mod init kxss &>/dev/null && go mod tidy &>/dev/null && 
-  go get github.com/Emoe/kxss && go install github.com/Emoe/kxss && sudo cp "$(pwd)/go/bin/kxss" /usr/local/go/bin/kxss
+  go get github.com/Emoe/kxss && go install github.com/Emoe/kxss && sudo cp "/root/go/bin/kxss" /usr/local/go/bin/kxss
   cd "$SCRIPT_DIR"  # Navigate back to the main installation directory
 fi
 
